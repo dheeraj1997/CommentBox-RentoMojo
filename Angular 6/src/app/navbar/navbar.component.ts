04c6c644fd1a68226caa1929e0728c92a0291f9b
dheeraj1997 <dheerajvarshney00@gmail.com>
2019-04-27 16:55:10 +0530

    navbar setup complete

--- Angular 6/src/app/navbar/navbar.component.ts
+++ Angular 6/src/app/navbar/navbar.component.ts
@@ -11,6 +11,9 @@ export class NavbarComponent implements OnInit {
 
   constructor(private userService: UserService, private router: Router) { }
 
+  ifLoggedIn(){
+    return this.userService.isLoggedIn();
+  }
   onLogout(){
     this.userService.deleteToken();
     this.router.navigate(['/']);
