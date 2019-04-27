f3cbc05ff9ab4734b5bc321cc33f15fb7b2cbe03
dheeraj1997 <dheerajvarshney00@gmail.com>
2019-04-27 19:29:33 +0530

    alerts set

--- Angular 6/src/app/comment/comment.component.ts
+++ Angular 6/src/app/comment/comment.component.ts
@@ -8,32 +8,40 @@ import { Router } from "@angular/router";
   styleUrls: ['./comment.component.css']
 })
 export class CommentComponent implements OnInit {
-
+  serverErrorMessages: string;
+  success: boolean;
   constructor(private userService: UserService, private router: Router) { }
-  
+
   ngOnInit() {
+  }
+  ifLoggedIn(){
+    return this.userService.isLoggedIn();
   }
 
   onComment(){
     console.log("comment is ",this.comment);
-    if(!this.comment){
-      this.flashMessage.show('No Comment ', {cssClass: 'alert-danger position-fixed ', timeout: 2000});
-    }
-    else if(!this.authService.loggedIn()){
-      this.flashMessage.show('Please Login ', {cssClass: 'alert-danger ', timeout: 2000});
+    if(!this.userService.isLoggedIn()){
+      // this.flashMessage.show('Please Login ', {cssClass: 'alert-danger ', timeout: 2000});
+      // this.serverErrorMessages = 'Please Login';
       this.router.navigate(['/login']);
     }
+    else if(!this.comment){
+      // this.flashMessage.show('No Comment ', {cssClass: 'alert-danger position-fixed ', timeout: 2000});
+      this.success = false;
+      this.serverErrorMessages = 'No Comment';
+    }
     else {
+      this.success = true;
+      this.serverErrorMessages = 'Comment posted successfully ';
       var com = this.comment;
       this.comment = '';
       this.authService.comment(com).subscribe(data => {
         console.log("comment posted ", data);
         //console.log("com",com);
         this.loadedComments.push(data);
-        this.flashMessage.show('Comment posted successfully ', {cssClass: 'alert-success position-fixed', timeout: 2000});
+        // this.flashMessage.show('Comment posted successfully ', {cssClass: 'alert-success position-fixed', timeout: 2000});
       })
     }
   }
-
 
 }
