7b184c65d6767f1c0d149f11a35e2b6786e34fcc
dheeraj1997 <dheerajvarshney00@gmail.com>
2019-04-27 18:24:05 +0530

    comment box ready

--- Angular 6/src/app/app.module.ts
+++ Angular 6/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // components
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { NavbarComponent } from './navbar/navbar.component';
 //routes
 import { appRoutes } from './routes';
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -16,7 +18,6 @@ import { UserService } from './shared/user.service';
 //other
 import { AuthGuard } from './auth/auth.guard';
 import { AuthInterceptor } from './auth/auth.interceptor';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
